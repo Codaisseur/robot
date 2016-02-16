7e2833b11d56f11ce8945f9c6ad02f5065d922a9
irisbune <irisbune@gmail.com>
2016-02-16 14:33:32 +0100

    added lunch thingy

--- scripts/iris.js
+++ scripts/iris.js
@@ -7,12 +7,14 @@ module.exports = function(robot){
   robot.hear(/(music DB|musicDB|homework)/i, function(msg){
     msg.reply("Oh no, more homework? Here's a pug to save the day :)");
     return msg.http("http://pugme.herokuapp.com/random").get()(function(err, res, body) {
-    return msg.send(JSON.parse(body).pug);
+      return msg.send(JSON.parse(body).pug);
+    })
   });
 
-
-
-
-  })
+  robot.hear(/(lunch)/i, function(msg){
+      return msg.http("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag&tag=lunch").get()(function(err, res, body){
+        return msg.send(JSON.parse(body).data.url);
+      })
+  });
 
 }
