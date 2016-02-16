b02cd1b90524c109e485929823a1c5894baef0b4
Miloucodaisseur <post.milou@gmail.com>
2016-02-16 12:43:25 +0100

    kim_robot

---
+++ scripts/kim.js
@@ -0,0 +1,7 @@
+module.exports = function(robot){
+  robot.hear(/.*kim.*/i, function(msg){
+    msg.reply('She is twerking');
+  })
+  robot.hear(/.*captain.*/i, function(msg){
+    msg.reply('What captain\? You are a lousy sailor');
+  })
